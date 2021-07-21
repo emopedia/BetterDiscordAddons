746e929f6052c1990c59b6a1cb8e84fbc5b917f6
emopedia <76584247+emopedia@users.noreply.github.com>
2021-07-21 16:04:49 +1200

    Add files via upload

--- Plugins/WritePeriod.plugin.js
+++ Plugins/WritePeriod.plugin.js
@@ -1,3 +1,13 @@
+/**
+ * @name WritePeriod
+ * @author emopedia
+ * @authorId 576333646865104897
+ * @version 1.0.0
+ * @description Adds a Period (Fullstop) to the end of a sentence.
+ * @source https://raw.githubusercontent.com/emopedia/BetterDiscordAddons/main/Plugins/WritePeriod.plugin.js
+ * @updateUrl https://mwittrien.github.io/BetterDiscordAddons/Plugins/WriteUpperCase/WriteUpperCase.plugin.js
+ */
+
 module.exports = (_ => {
 	const config = {
 		"info": {
